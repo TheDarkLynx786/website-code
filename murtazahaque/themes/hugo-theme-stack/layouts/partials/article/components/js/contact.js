7294ef4930a42b483ab4d5d8c54d1ea482e0029b
Murtaza Haque <MurtazaHaque@my.unt.edu>
2024-08-07 12:00:24 -0500

    More changes, more fixes
    Still doesn't work, I'm growing desperate

--- murtazahaque/themes/hugo-theme-stack/layouts/partials/article/components/js/contact.js
+++ murtazahaque/themes/hugo-theme-stack/layouts/partials/article/components/js/contact.js
@@ -1,29 +1,33 @@
 (function() {
     // https://dashboard.emailjs.com/admin/account
-    emailjs.init({publicKey: "Xw75qd5ide9C3mChA",});
+    emailjs.init({publicKey: "GSwdno2FHiciyvCb4",});
 })();
 
 
 const msg = document.querySelector(".sendoff-msg");
 
 window.onload = function() {
-    document.getElementById('form').addEventListener('submit', function(event) {
+    document.getElementById("form").addEventListener('submit', function(event) {
+        
         event.preventDefault();
+        
         document.querySelector(".loader").classList.add("show");
+
         // these IDs from the previous steps
         emailjs.sendForm('service_my_website', 'template_website_email', this).then(
             () => {
-                document.getElementById('form').reset();
+                document.getElementById("form").reset();
                 document.querySelector(".loader").classList.remove("show");
-                msg.innerHTML = ""
+                msg.innerHTML = "";
                 msg.innerHTML += "<span class='msg-sent'>Email Sent! Thanks for contacting!</span>";
-                msg.classList.add("show")
-                setTimeout(() => msg.classList.remove("show"), 5000);
+                msg.classList.add("show");
+                setTimeout(() => msg.classList.remove("show"), 2000);
             }, 
             (error) => {
                 document.querySelector(".loader").classList.toggle("show");
+                msg.innerHTML = "";
                 msg.innerHTML += "<span class='msg-not-sent'>Uh Oh! Email Not Sent, Please Try Again.</span>";
-                msg.classList.add("show")
+                msg.classList.add("show");
             }
         );
     });
